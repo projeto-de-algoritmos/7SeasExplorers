0e0c33b47bce521ea0539466c363471afed48cd9
Samuel Avila <samuel2001.furtadoavila@gmail.com>
2023-02-06 18:23:17 -0300

    refactor map graph

--- app/src/utils/mapGraph.js
+++ app/src/utils/mapGraph.js
@@ -1,134 +1,314 @@
 export const mapGraph = {
     Gurluk:
     {
-        Stein: 320,
-        Berg: 893,
+        Stein: {
+            distance: 320,
+        },
+        Berg: {
+            distance: 893,
+        },
     },
     Stein:
     {
-        Gurluk: 320,
-        Berg: 812,
-        Tuy: 1373,
+        Gurluk: {
+            distance: 320,
+        },
+        Berg: {
+            distance: 812,
+        },
+        Tuy: {
+            distance: 1373,
+        },
     },
     Berg:
     {
-        Stein: 812,
-        Gurluk: 893,
-        HaThuy: 1965,
-        Tuy: 988, 
+        Stein: {
+            distance: 812,
+        },
+        Gurluk: {
+            distance: 893,
+        },
+        HaThuy: {
+            distance: 1965,
+        },
+        Tuy: {
+            distance: 988,
+        }, 
     },
     Tuy:
     {   
-        Stein: 1373,
-        Berg: 988,
-        Trang: 397,
-        HueTra: 538,
-        HaThuy: 776,
-        Castro: 931,
-        Myoporizi: 1724,
-        Namely: 1348,
-        Komendrod: 1252,
-        Derencsza: 1445,
+        Stein: {
+            distance: 1373,
+        },
+        Berg: {
+            distance: 988,
+        },
+        Trang: {
+            distance: 397,
+        },
+        HueTra: {
+            distance: 538,
+        },
+        HaThuy: {
+            distance: 776,
+        },
+        Castro: {
+            distance: 931,
+        },
+        Myoporizi: {
+            distance: 1724,
+        },
+        Namely: {
+            distance: 1348,
+        },
+        Komendrod: {
+            distance: 1252,
+        },
+        Derencsza: {
+            distance: 1445,
+        },
         },
     Trang:
     {
-        Tuy: 397,
-        HueTra: 705,
-        HaThuy: 862,
-        Castro: 1018,
-        Namely: 1947,
-        Komendrod: 1430,
-        Derencsza: 1632,
-        Myoporizi: 1892,
+        Tuy: {
+            distance: 397,
+        },
+        HueTra: {
+            distance: 705,
+        },
+        HaThuy: {
+            distance: 862,
+        },
+        Castro: {
+            distance: 1018,
+        },
+        Namely: {
+            distance: 1947,
+        },
+        Komendrod: {
+            distance: 1430,
+        },
+        Derencsza: {
+            distance: 1632,
+        },
+        Myoporizi: {
+            distance: 1892,
+        },
     },
     HueTra:
     {
-        Trang: 705,
-        Tuy: 538,
-        HaThuy: 423,
-        Castro: 608,
-        Namely: 1657,
-        Komendrod: 1579,
-        Derencsza: 1178,
-        Myoporizi: 2036,
+        Trang: {
+            distance: 705,
+        },
+        Tuy: {
+            distance: 538,
+        },
+        HaThuy: {
+            distance: 423,
+        },
+        Castro: {
+            distance: 608,
+        },
+        Namely: {
+            distance: 1657,
+        },
+        Komendrod: {
+            distance: 1579,
+        },
+        Derencsza: {
+            distance: 1178,
+        },
+        Myoporizi: {
+            distance: 2036,
+        },
     },
     HaThuy:
     {
-        HueTra: 423,
-        Castro: 289,
-        Trang: 1001,
-        Tuy: 776, 
-        Berg: 1965,
-        Namely: 1854,
-        Myoporizi: 2287,
-        Komendrod: 1782,
-        Derencsza: 1990,
+        HueTra: {
+            distance: 423,
+        },
+        Castro: {
+            distance: 289,
+        },
+        Trang: {
+            distance: 1001,
+        },
+        Tuy: {
+            distance: 776,
+        }, 
+        Berg: {
+            distance: 1965,
+        },
+        Namely: {
+            distance: 1854,
+        },
+        Myoporizi: {
+            distance: 2287,
+        },
+        Komendrod: {
+            distance: 1782,
+        },
+        Derencsza: {
+            distance: 1990,
+        },
     },
     Castro:
     {
-        HaThuy: 289,
-        Tuy: 931,
-        Trang: 1018,
-        Lutunum: 2177,
-        Myoporizi: 1063,
-        Komendrod: 1849,
-        Derencsza: 2044, 
-        HueTra: 608,
+        HaThuy: {
+            distance: 289,
+        },
+        Tuy: {
+            distance: 931,
+        },
+        Trang: {
+            distance: 1018,
+        },
+        Lutunum: {
+            distance: 2177,
+        },
+        Myoporizi: {
+            distance: 1063,
+        },
+        Komendrod: {
+            distance: 1849,
+        },
+        Derencsza: {
+            distance: 2044,
+        }, 
+        HueTra: {
+            distance: 608,
+        },
     },
     Myoporizi:
     {
-        Derencsza: 930,
-        Komendrod: 753,
-        Namely: 1343,
-        Lutunum: 1708,
-        Castro: 1063,
-        Tuy: 1724, 
-        Trang: 1892,
-        HueTra: 2036,
-        HaThuy: 2287,
+        Derencsza: {
+            distance: 930,
+        },
+        Komendrod: {
+            distance: 753,
+        },
+        Namely: {
+            distance: 1343,
+        },
+        Lutunum: {
+            distance: 1708,
+        },
+        Castro: {
+            distance: 1063,
+        },
+        Tuy: {
+            distance: 1724,
+        }, 
+        Trang: {
+            distance: 1892,
+        },
+        HueTra: {
+            distance: 2036,
+        },
+        HaThuy: {
+            distance: 2287,
+        },
     },
     Komendrod:
     {
-        Castro: 1849,
-        Tuy: 1252,
-        Trang: 1430,
-        HueTra: 1579,  
-        HaThuy: 1782,
-        Myoporizi: 753,
-        Namely: 913,   
-        Derencsza: 381,
+        Castro: {
+            distance: 1849,
+        },
+        Tuy: {
+            distance: 1252,
+        },
+        Trang: {
+            distance: 1430,
+        },
+        HueTra: {
+            distance: 1579,
+        },  
+        HaThuy: {
+            distance: 1782,
+        },
+        Myoporizi: {
+            distance: 753,
+        },
+        Namely: {
+            distance: 913,
+        },   
+        Derencsza: {
+            distance: 381,
+        },
     },
     Derencsza:
     {
-        Komendrod: 381,
-        Myoporizi: 930,
-        Namely: 1112,
-        Tuy: 1445, 
-        Trang: 1632,
-        HueTra: 1178,
-        HaThuy: 1990,
-        Castro: 2044,
+        Komendrod: {
+            distance: 381,
+        },
+        Myoporizi: {
+            distance: 930,
+        },
+        Namely: {
+            distance: 1112,
+        },
+        Tuy: {
+            distance: 1445,
+        }, 
+        Trang: {
+            distance: 1632,
+        },
+        HueTra: {
+            distance: 1178,
+        },
+        HaThuy: {
+            distance: 1990,
+        },
+        Castro: {
+            distance: 2044,
+        },
     },
     Namely:
     {
-        Hajdu: 497,
-        Komendrod: 913,
-        Derencsza: 1112,
-        Myoporizi: 1343,
-        Tuy: 1348,
-        Trang: 1947,
-        HaThuy: 1854,
-        HueTra: 1657,
+        Hajdu: {
+            distance: 497,
+        },
+        Komendrod: {
+            distance: 913,
+        },
+        Derencsza: {
+            distance: 1112,
+        },
+        Myoporizi: {
+            distance: 1343,
+        },
+        Tuy: {
+            distance: 1348,
+        },
+        Trang: {
+            distance: 1947,
+        },
+        HaThuy: {
+            distance: 1854,
+        },
+        HueTra: {
+            distance: 1657,
+        },
     },
     Hajdu:
     {
-        Namely: 497,
-        Lutunum: 911,
+        Namely: {
+            distance: 497,
+        },
+        Lutunum: {
+            distance: 911,
+        },
     },
     Lutunum:
     {
-        Myoporizi: 1708,
-        Castro: 2177,
-        Hajdu: 911,
+        Myoporizi: {
+            distance: 1708,
+        },
+        Castro: {
+            distance: 2177,
+        },
+        Hajdu: {
+            distance: 911,
+        },
     },
 }
